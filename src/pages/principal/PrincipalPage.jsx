2462e78377c074977edba6fe4ac8237fa43459df
Epereira2021576 <134961772+Epereira2021576@users.noreply.github.com>
2024-05-03 03:09:32 -0600

    commit

--- src/pages/principal/PrincipalPage.jsx
+++ src/pages/principal/PrincipalPage.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import { TaskForm } from '../../components/taskForm'
 import "./PrincipalPage.css"
 import { getTasks as getTasksRequest } from '../../services/api.jsx'
+import { BsCircleFill, BsTrashFill } from "react-icons/bs";
 
 export const PrincipalPage = () => {
     const [tasks, setTasks] = useState( [] )
@@ -11,7 +12,7 @@ export const PrincipalPage = () => {
             if ( response.data ) {
                 setTasks( response.data )
             }
-        } )
+        } ).catch( err => console.log( err ) )
     } )
     return (
         <>
@@ -21,13 +22,19 @@ export const PrincipalPage = () => {
                 {
                     tasks.length === 0 ? <h2>No hay tareas</h2> :
                         tasks.map( ( task, index ) => (
-                            <div key={index}>
-                                <h2>{task.nombreTarea}</h2>
-                                <p>{task.descripcion}</p>
-                                <p>{task.fechaDeInicio}</p>
-                                <p>{task.fechaDeCierre}</p>
-                                <p>{task.name}</p>
-                                <p>{task.lastName}</p>
+                            <div key={index} className="task">
+                                <div className="checkbox">
+                                    <BsCircleFill className="icon" />
+                                    <h2>{task.nombreTarea}</h2>
+                                    <p>{task.descripcion}</p>
+                                    <p>{task.fechaDeInicio}</p>
+                                    <p>{task.fechaDeCierre}</p>
+                                    <p>{task.name}</p>
+                                    <p>{task.lastName}</p>
+                                </div>
+                                <div>
+                                    <span><BsTrashFill className="icon" /></span>
+                                </div>
                             </div>
                         ) )
                 }
